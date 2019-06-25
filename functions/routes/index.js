0a45cf268c8f3c9ff13c30cfa7c7c665689eac80
Simon Opheim <simon.opheim@gmail.com>
2019-06-26 00:47:01 +0200

    added possibility to supply own list

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const validateRequest = require('../middleware/validateRequest');
 const createCloud = require('../controllers/create-cloud');
+// const countWords = require('../controllers/count-words')
+
 const router = express.Router();
 
 //NB! All routes are dependent on config in firebase.json, under rewrites.
@@ -10,6 +12,16 @@ router.post('/api/create-cloud',
     validateRequest,
     createCloud
     )
+
+// router.post('/api/count-words',
+//     validateRequest,
+//     countWords
+//     )
+
+// router.post('api/count-words-web',
+//     //validateRequest,
+//     countWordsWeb
+// )
 
 router.use((err, req, res, next) => {
     if (err.message === "Bad Request") {
