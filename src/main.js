2ad28dfe1128b9643736d9b38a10d9b17fc70b38
Simon Opheim <simon.opheim@gmail.com>
2019-06-25 14:43:27 +0200

    removed empty words

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ form.addEventListener("submit", (e) => {
 function countWords(string) {
     const regExp = /\S+/gi;
     const wordsRaw = string.toLowerCase().match(regExp);
-    const words = wordsRaw.map(word => word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?]/g, ""));
+    const words = wordsRaw.map(word => word.replace(/[.,\/#!$%\^&\*;:{}=\-_`'´~()?]/g, "")).filter(word => word.length > 0);
     const count = {};
 
     if(isIterable(words)) { //checks if there are no words and throws an error if there are no words
