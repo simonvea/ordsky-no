e4b8616e8865186efad4f3a3dcfe22c90f7c79e3
Simon Opheim <simon.opheim@attentec.no>
2020-04-13 22:31:13 +0200

    add config to cloud route

--- functions/controllers/cloud.js
+++ functions/controllers/cloud.js
@@ -1,30 +1,28 @@
-const cloud = require("d3-cloud");
-const { createCanvas } = require("canvas");
+const cloud = require('d3-cloud');
+const { createCanvas } = require('canvas');
 const addAttr = require('../utils/add-attributes');
 
 module.exports = (req, res, next) => {
+  const { words, config } = req.body;
 
-    const words = req.body.words;
+  if (!words) return next('NO WORDS');
 
-    if(!words) return next('NO WORDS')
+  const svgWidth = config.svgWidth || 500;
+  const svgHeight = config.svgHeight || 500;
+  const paddingBetweenWords = config.padding || 2;
+  const rotationDeg = config.rotation || ~~(Math.random() * 4) * 45 - 45;
+  const font = config.font || 'Impact';
 
-    const svgWidth = req.body.svgWidth || 500;
-    const svgHeight = req.body.svgHeight || 500;
-    const paddingBetweenWords = req.body.padding || 7;
-    const rotationDeg = req.body.rotation || 90;
-    const font = req.body.font || "Impact";
+  const wordsWithAttr = addAttr(words); //Returning an array of objects with the form: {text: "word", size: 50}
 
-    const wordsWithAttr = addAttr(words); //Returning an array of objects with the form: {text: "word", size: 50}
-    
-    cloud()
-      .size([svgWidth,svgHeight])
-      .canvas(() => createCanvas(1, 1))
-      .words(wordsWithAttr) 
-      .padding(paddingBetweenWords)
-      .rotate(() => ~~(Math.random() * 4) * 45 - 45)
-      .font(font)
-      .fontSize(d => d.size)
-      .on("end", wordsFinished => res.send(JSON.stringify(wordsFinished)))
-      .start();
-
-  }
+  cloud()
+    .size([svgWidth, svgHeight])
+    .canvas(() => createCanvas(1, 1))
+    .words(wordsWithAttr)
+    .padding(paddingBetweenWords)
+    .rotate(rotationDeg)
+    .font(font)
+    .fontSize((d) => d.size)
+    .on('end', (wordsFinished) => res.send(JSON.stringify(wordsFinished)))
+    .start();
+};
